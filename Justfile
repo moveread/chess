23b9300d67e0307ef7d6ba2a1eda4d2fa2e31780
m4rs <marcel@marcelclaramunt.cat>
2024-03-31 11:03:02 +0200

    chess-notation

--- Justfile
+++ Justfile
@@ -1,2 +1,12 @@
 mod pyutils "python/chess-utils/Justfile"
+mod pynotation "python/chess-notation/Justfile"
 mod jsutils "js/chess-utils/Justfile"
+
+VENV := ".venv"
+PYTHON := ".venv/bin/python"
+
+init:
+  rm -drf {{VENV}} || :
+  python3.11 -m venv {{VENV}}
+  {{PYTHON}} -m pip install --upgrade pip
+  {{PYTHON}} -m pip install -r requirements.txt
--- python/chess-utils/src/chess_utils/parse/parse.py
+++ python/chess-utils/src/chess_utils/parse/parse.py
@@ -16,9 +16,10 @@ def uci(e2e4: str) -> chess.Move:
   """Like `chess.Move.from_uci` but ~50% faster"""
   try:
     e2 = e2e4[:2]
-    e4 = e2e4[2:]
-    return chess.Move(square(e2), square(e4))
+    e4 = e2e4[2:4]
+    promotion = chess.PIECE_SYMBOLS.index(e2e4[4]) if len(e2e4) == 5 else None
+    return chess.Move(square(e2), square(e4), promotion=promotion)
   except Exception as e:
     import logging
     logging.getLogger().error(f"Exception parsing uci '{e2e4}'", e)
-    return chess.Move.from_uci(uci)
+    return chess.Move.from_uci(e2e4)
