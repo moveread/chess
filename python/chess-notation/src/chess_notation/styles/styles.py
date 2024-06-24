4d8cea5c86500d6562b8943a14a6a38d926e6e05
m4rs <marcel@marcelclaramunt.cat>
2024-06-24 20:13:05 +0200

    chess-notation +=  N/As

--- python/chess-notation/src/chess_notation/styles/styles.py
+++ python/chess-notation/src/chess_notation/styles/styles.py
@@ -1,4 +1,5 @@
 from typing import Literal
+from pydantic import BaseModel
 
 Check = Literal["NONE", "CHECK"]
 Mate = Literal["NONE", "SHARP", "DOUBLE_CHECK"]
@@ -15,3 +16,21 @@ Piece capture styles that convey unique information (start file only, end file o
 I.e. no two styles include the same info
 """
 UNIQ_PIECE_CAPTURES: list[PieceCapture] = ['Ne4', 'Nxe4', 'NxN']
+
+NA = Literal['N/A']
+
+def no_na(value):
+  if value != 'N/A':
+    return value
+
+class KingEffects(BaseModel):
+  check: Check | NA | None = None
+  mate: Mate | NA | None = None
+
+class Motions(BaseModel):
+  castle: Castle | NA | None = None
+  pawn_capture: PawnCapture | NA | None = None
+  piece_capture: PieceCapture | NA | None = None
+
+class Styles(KingEffects, Motions):
+  ...
